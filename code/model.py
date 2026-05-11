9cc0f81a58a9fdd235a28203f1bbccb8c4915d63
sgf2001 <122586084+sgf2001@users.noreply.github.com>
2026-05-11 20:10:38 +0800

    Update model.py

--- code/model.py
+++ code/model.py
@@ -18,15 +18,10 @@ class GlycoproteinProphet(nn.Module):
         self.glycan_rnn = nn.RNN(64, 64, 2)
         self.glycan_f2 = nn.Linear(402, 32)
         self.bn_glycan1 = nn.BatchNorm1d(32)
-
-        #self.bn_fc1 = nn.Linear(64, 32)
-        #medthod1
         self.bn_fc1 = nn.Linear(64, 32)
         self.bn_fc2 = nn.Linear(32, 16)
         self.bn_fc3 = nn.Linear(16, 1)
         self.bn_relu = nn.ReLU()
-        # Attention
-
         self.W_query = nn.Linear(402, 64)
         self.W_key = nn.Linear(402, 64)
         self.W_value = nn.Linear(402, 64)
@@ -42,40 +37,15 @@ class GlycoproteinProphet(nn.Module):
         x = self.prot_fc1(prot_X_train_tensor)
         prot1 = self.bn_prot1(self.prot_dropout1(x))
         prot2 = self.bn_prot2(self.prot_dropout2(self.prot_fc2(prot1)))
-        #LSTM
-        #glycan1,_= self.glycan_lstm(self.glycan_fc1(glycan_X_train_tensor))
-        #glycan_X_train_tensor = glycan_X_train_tensor.transpose(0,1)
-        # CNN
-        # glycan1 = self.conv1(glycan_X_train_tensor)
-        # glycan1 = glycan1.transpose(0, 1)
-        # glycan1,_=self.glycan_rnn(glycan1)
-        # glycan1 = glycan1.transpose(0, 1)
-        # glycan1 = self.conv2(glycan1)
-        # glycan1 = glycan1.transpose(0, 1)
-        # glycan1 = self.conv1(self.glycan_fc1(glycan_X_train_tensor).transpose(0,1)).transpose(0,1)
-        # #glycan2 = self.pool(glycan1).transpose(0,1)
-        # #glycan2 = glycan2.view(glycan2.size(0), -1)
-        # glycan3 = self.bn_glycan1(self.activation_fn(self.glycan_f2(glycan1)))
-        # LSTM+RNN
-        # glycan1, _ = self.glycan_lstm(glycan_X_train_tensor)
-        # # glycan2,_ = self.glycan_rnn(glycan1)
-        #glycan3 = self.bn_glycan1(self.glycan_f2(glycan1))
-        # Attention
         query = self.W_query(glycan_X_train_tensor)
         key = self.W_key(glycan_X_train_tensor)
         value = self.W_value(glycan_X_train_tensor)
         key = key.transpose(0, 1)
         scores = torch.matmul(query, key)
-        # attention_weights = torch.exp(scores)/torch.sum(scores, dim=1, keepdim=True)
-        # attention_weights = self.softmax(scores)
         attention_weights = F.normalize(scores, p=2, dim=1)
         weighted_values = torch.matmul(attention_weights, value)
-        #print(weighted_values.shape)
         glycan_feature = torch.cat((glycan_X_train_tensor, weighted_values), dim=1)
         glycan = self.attention_glycan_fc1(glycan_feature)
-        #glycan = self.glycan_f2(glycan_X_train_tensor)
         h_n = torch.cat((prot2, glycan), 1)
-        #x = F.sigmoid(self.bn_fc1(h_n))
         x = F.sigmoid(self.bn_fc3(self.bn_relu(self.bn_fc2(self.bn_relu(self.bn_fc1(h_n))))))
-        #x = nn.functional.softmax(self.bn_fc3(self.bn_relu(self.bn_fc2(self.bn_relu(self.bn_fc1(h_n))))))
         return x
