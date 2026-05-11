d5c3c4dcd162e244a4d43e288cac708da4ca7bc0
sgf2001 <122586084+sgf2001@users.noreply.github.com>
2026-05-11 19:40:24 +0800

    Update sugar_7.py

--- glycan_encode/sugardir/sugar_7.py
+++ glycan_encode/sugardir/sugar_7.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2023/6/14 20:55
-# @Author  : lijianxin
-# @File    : sugar_7.py
-# @Software: PyCharm
 import sys
 
 
@@ -33,50 +28,45 @@ def builtGlycan7Node(label1, sugarlabvalue):
     elif label1[-2] == "c":
         Glycan7Node.anomer["c"] = "c"
     else:
-        print("annomer，不合法，请检查语法")
         sys.exit()
 
-    # 需要对根节点处理
+
     try:
         if label1[-1] == "2":
             Glycan7Node.parent.append("2")
         elif label1[-1] == "0":
             Glycan7Node.parent = "root"
     except IndexError:
-        print("父节点出现错误")
         sys.exit()
-    # 需要判断对那个孩子标记，这种好像适合类似4Glca1的节点
 
     Modification_groups = label1[1:len(label1[1:-2]) - len(sugarlabvalue) + 1]
     search_index = []
     if 'S' or 's' in Modification_groups:
         for index, letter in enumerate(Modification_groups):
             if letter == 'S':
-                search_index.append(Modification_groups[index - 1])  # [1]
+                search_index.append(Modification_groups[index - 1])  
             for SO3_index in search_index:
                 for chidrenkey, chidrenvalue in Glycan7Node.childrenlistdic.items():
                     if SO3_index == chidrenkey:
                         chidrenvalue.append("SO3")
                 search_index = []
-        # 判断是否含P
+  
     if 'P' or 'p' in Modification_groups:
         for index, letter in enumerate(Modification_groups):
             if letter == 'P':
-                search_index.append(Modification_groups[index - 1])  # [1]
+                search_index.append(Modification_groups[index - 1]) 
                 for p_indx in search_index:
                     for chidrenkey, chidrenvalue in Glycan7Node.childrenlistdic.items():
                         if p_indx == chidrenkey:
                             chidrenvalue.append("P")
                     search_index = []
     if label1[0]!='0' and label1[0] not in Glycan7Node.childrenlistdic.keys():
-        print(label1[0] + ':有问题')
+        print(label1[0] + ':error')
         sys.exit()
     for childkey, childvalue in Glycan7Node.childrenlistdic.items():
         if label1[0] == childkey and len(childvalue) == 0:
             childvalue.append(childkey)
             Glycan7Node.childflagnum += 1
-    # if Glycan7Node.childflagnum == 0:
-    #     print("此节点是叶子节点，没有链接孩子")
 
     return Glycan7Node
 
