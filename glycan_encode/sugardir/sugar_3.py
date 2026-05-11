981ea4bb012ad5b382538064cdcd865c36cc92f8
sgf2001 <122586084+sgf2001@users.noreply.github.com>
2026-05-11 18:25:26 +0800

    Update sugar_3.py

--- glycan_encode/sugardir/sugar_3.py
+++ glycan_encode/sugardir/sugar_3.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2023/6/14 20:48
-# @Author  : lijianxin
-# @File    : sugar_3.py
-# @Software: PyCharm
 import sys
 
 
@@ -33,33 +28,28 @@ def builtGlycan3Node(label1, sugarlabvalue):
     elif label1[-2] == "c":
         Glycan3Node.anomer["c"] = "c"
     else:
-        print("annomer，不合法，请检查语法")
         sys.exit()
 
-    # 需要对根节点处理
     try:
         if label1[-1] == "1":
             Glycan3Node.parent.append("1")
         elif label1[-1] == "0":
             Glycan3Node.parent = "root"
     except IndexError:
-        print("父节点出现错误")
         sys.exit()
-    # 这地方可以统计出标记的数量
 
-    # 判断是否含S
     Modification_groups = label1[1:len(label1[1:-2]) - len(sugarlabvalue) + 1]
     search_index = []
     if 'S' or 's' in Modification_groups:
         for index, letter in enumerate(Modification_groups):
             if letter == 'S':
-                search_index.append(Modification_groups[index - 1])  # [1]
+                search_index.append(Modification_groups[index - 1])  
             for SO3_index in search_index:
                 for chidrenkey, chidrenvalue in Glycan3Node.childrenlistdic.items():
                     if SO3_index == chidrenkey:
                         chidrenvalue.append("SO3")
                 search_index = []
-        # 判断是否含P
+
     if 'P' or 'p' in Modification_groups:
         for index, letter in enumerate(Modification_groups):
             if letter == 'P':
@@ -70,14 +60,12 @@ def builtGlycan3Node(label1, sugarlabvalue):
                             chidrenvalue.append("P")
                     search_index = []
     if label1[0]!='0' and label1[0] not in Glycan3Node.childrenlistdic.keys():
-        print(label1[0] + ':有问题')
+ 
         sys.exit()
     for childkey, childvalue in Glycan3Node.childrenlistdic.items():
         if label1[0] == childkey and len(childvalue) == 0:
             childvalue.append(childkey)
             Glycan3Node.childflagnum += 1
-    # if Glycan3Node.childflagnum == 0:
-    #     print("此节点是叶子节点，没有链接孩子")
 
     return Glycan3Node
 
